0c46240caad3e244011d1968dd4be6f5adaccbc4
juniorsolo <junior.solo@hotmail.com>
2023-07-04 17:47:47 -0300

    Implement find by id for client.

--- src/main/java/com/junior/atendimento/controllers/ClienteController.java
+++ src/main/java/com/junior/atendimento/controllers/ClienteController.java
@@ -1,11 +1,12 @@
 package com.junior.atendimento.controllers;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -25,6 +26,18 @@ public class ClienteController {
 		if(list != null) {
 			Gson gson = new Gson();
 			String json = gson.toJson(list);
+			return ResponseEntity.ok().body(json);
+		}
+		return ResponseEntity.badRequest().body("Error");
+	}
+	
+	@GetMapping(value = "client/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
+	public ResponseEntity<String> getClientById(@PathVariable Integer id) {
+
+		Optional<Client> optional = clientService.getById(id);
+		if(optional.isPresent()) {
+			Gson gson = new Gson();
+			String json = gson.toJson(optional.get());
 			return ResponseEntity.ok().body(json);
 		}
 		return ResponseEntity.badRequest().body("Error");
